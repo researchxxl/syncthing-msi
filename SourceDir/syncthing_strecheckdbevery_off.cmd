60013f7122d021fae4fd2cbde0593500ab5eb01f
researchxxl <244199422+researchxxl@users.noreply.github.com>
2026-01-09 13:14:19 +0100

    chore(wxs): Remove syncthing_strecheckdbevery scripts

--- SourceDir/syncthing_strecheckdbevery_off.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-REM 
-REG DELETE "HKLM\System\CurrentControlSet\Services\Syncthing\Parameters" /v "AppEnvironmentExtra" /f
-REM 
-timeout 3
-goto :eof
--- SourceDir/syncthing_strecheckdbevery_on.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-REM 
-REG ADD "HKLM\System\CurrentControlSet\Services\Syncthing\Parameters" /v "AppEnvironmentExtra" /t REG_MULTI_SZ /d "STRECHECKDBEVERY=1s\0" /f
-REM 
-timeout 3
-goto :eof
